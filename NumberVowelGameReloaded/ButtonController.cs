62dfc7a0d38ba8b458f81adb52fbb0998e5ab93f
BuildTools <unconfigured@null.spigotmc.org>
2015-10-22 17:19:44 +0200

    Push

--- NumberVowelGameReloaded/ButtonController.cs
+++ NumberVowelGameReloaded/ButtonController.cs
@@ -8,6 +8,14 @@ namespace NumberVowelGameReloaded
 {
     public class ButtonController
     {
+        NumLetterController _NumLetterController;
+        NumLetterView _NumLetterView;
+
+        public ButtonController(NumLetterController NumLetterController)
+        {
+            _NumLetterController = NumLetterController;
+            _NumLetterView = _NumLetterController._view;
+        }
 
         public void checkAnswer(string nummerLetter,string BovenOfOnder)
         {
