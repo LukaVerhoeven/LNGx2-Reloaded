2f06e5a330961535272b5a51462e12a0c31136b3
Alax <barratt90@gmail.com>
2015-10-22 17:17:00 +0200

    Visual Push

--- NumberVowelGameReloaded/NumLetterView.Designer.cs
+++ NumberVowelGameReloaded/NumLetterView.Designer.cs
@@ -35,11 +35,11 @@
             // 
             // label1
             // 
-            this.label1.BackColor = System.Drawing.Color.Green;
+            this.label1.BackColor = System.Drawing.Color.PaleTurquoise;
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label1.Font = new System.Drawing.Font("Tahoma", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(44, 31);
+            this.label1.Location = new System.Drawing.Point(21, 16);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(209, 109);
@@ -48,11 +48,11 @@
             // 
             // label2
             // 
-            this.label2.BackColor = System.Drawing.Color.Green;
+            this.label2.BackColor = System.Drawing.Color.PaleTurquoise;
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label2.Font = new System.Drawing.Font("Tahoma", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(44, 172);
+            this.label2.Location = new System.Drawing.Point(21, 142);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(209, 109);
@@ -61,12 +61,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(89, 331);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button1.Location = new System.Drawing.Point(21, 267);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(209, 87);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "RUN";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -74,12 +75,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "NumLetterView";
-            this.Size = new System.Drawing.Size(609, 399);
+            this.Size = new System.Drawing.Size(247, 373);
             this.Load += new System.EventHandler(this.NumLetterView_Load);
             this.ResumeLayout(false);
 
